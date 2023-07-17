0aa8b9d07e48d0d66ce7ae095b9a21d440efea9c
Harsh Yadav <75542099+Harsh-Yadav-02@users.noreply.github.com>
2023-07-17 19:11:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function SkillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
