fef679797421a9e3c95aeaf83a85355ba0c9e87a
Harsh Yadav <75542099+Harsh-Yadav-02@users.noreply.github.com>
2023-07-17 19:26:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,11 @@
-// Import required modules
 const express = require('express');
-
-// Create an instance of Express
 const app = express();
-const port = 3000; // You can change this port to any available port you prefer
+const port = 3000;
 
-// Define a basic route
 app.get('/', (req, res) => {
   res.send('Hello, this is your web server!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
